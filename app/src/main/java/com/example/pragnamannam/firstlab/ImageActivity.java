9056508b694650b7cd72821ff4b1e2faef7de6f7
Pragna Mannam <pragnamannam@Pragnas-Air.wv.cc.cmu.edu>
2017-02-05 16:23:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pragnamannam/firstlab/ImageActivity.java
@@ -0,0 +1,47 @@
+package com.example.pragnamannam.firstlab;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+/**
+ * Created by pragnamannam on 1/30/17.
+ */
+public class ImageActivity extends AppCompatActivity {
+
+    Button imButton;
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.imageprocess);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
